1dbb8d78cb54bbe77d93d27442785c1605000bd8
Noah Vaters <144738466+NoV8rs@users.noreply.github.com>
2025-11-21 02:30:12 -0400

    Remove unused using directives
    Cleaned up Achievement_Manager.cs by removing unnecessary using statements for Enum, System, Collections.Generic, and Linq.

--- Achievement_Manager.cs
+++ Achievement_Manager.cs
@@ -1,9 +1,4 @@
-﻿using Enum = System.Enum;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace AchievementSystem
+﻿namespace AchievementSystem
 {
     // ---------------------------------------------------------
     // ACHIEVEMENT CLASSES
